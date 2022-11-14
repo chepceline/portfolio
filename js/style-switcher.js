389eec7ef237d969cb9114cc77d5a692ffa7e80c
celine <chepceline25@gmail.com>
2022-11-14 11:08:26 +0300

    add else statements

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -3,3 +3,25 @@ const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
+// Hide style switcher on scroll
+window.addEventListener("scroll", () =>{
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+/* theme colors */
+const alternateStyles = document.querySelector("alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) =>{
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
